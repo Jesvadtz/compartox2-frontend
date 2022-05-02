02fc7dc07a35c1a71e90d07318d629f6cb7e813b
Jessica <jesvadtz@gmail.com>
2022-05-02 00:51:26 -0500

    Add login and signup screens

--- components/Note/Note.js
+++ components/Note/Note.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 import styles from "./Note.module.scss";
 
-export default function Note({ note }) {
+export default function Note({ note, href }) {
   return (
     <div className={styles.note}>
-      <Link href="/">{note}</Link>
+      <Link href={href}>{note}</Link>
     </div>
   );
 }
