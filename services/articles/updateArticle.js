aefe5a4caf8dbad3b0e8ec7efc47e74d968e90f7
Jessica <jesvadtz@gmail.com>
2022-05-13 02:17:08 -0500

    Edit and delete article

--- services/articles/updateArticle.js
+++ services/articles/updateArticle.js
@@ -2,6 +2,7 @@ const HOST = process.env.NEXT_PUBLIC_BACKEND || "";
 
 const updateArticle = async (formData, articleId) => {
   try {
+    const user = JSON.parse(localStorage.getItem("user"));
     const result = await fetch(`${HOST}/articles/${articleId}`, {
       method: "PATCH",
       body: formData,
